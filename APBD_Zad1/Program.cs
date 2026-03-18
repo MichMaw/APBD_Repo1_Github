96fc8988092b135267491e5d44bfd1840a7a45e9
Michał Mach <s30662@pjwstk.edu.pl>
2026-03-19 03:55:25 +0800

    Added user input

--- APBD_Zad1/Program.cs
+++ APBD_Zad1/Program.cs
@@ -1,3 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
-
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Podaj imie:");
+string input = Console.ReadLine();
+Console.WriteLine("Czesc "+input);
